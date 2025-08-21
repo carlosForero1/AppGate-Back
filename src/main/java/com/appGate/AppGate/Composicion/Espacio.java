5e1f7b0ab047bd99cbd758ee53a3b7aeed2a7cdf
= <=>
2025-08-21 11:47:32 -0500

    - prueba positiva regular

--- src/main/java/com/appGate/AppGate/Composicion/Espacio.java
+++ src/main/java/com/appGate/AppGate/Composicion/Espacio.java
@@ -12,7 +12,7 @@ public class Espacio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String nombre  ;
+    private String nombre;
 
     private boolean verdadero ;
 
